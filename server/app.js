25e57c72266e0f2495972ea3b52557aa6191f86e
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-03-29 22:41:26 +0700

    get all users into mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const userRouter = require("./src/routes/user.route");
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -9,5 +14,9 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,19 @@
-exports.getAllUsers = async (req, res, next) => {
-  // const users = await User.find();
-  res.status(200).json({
-    status: "success",
-  });
+const User = require("./../models/user.model");
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
